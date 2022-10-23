8d586dbd69e573be3a49d456eeb2a0bb9bf44e68
Zhenyu1302 <88371904+Zhenyu1302@users.noreply.github.com>
2022-10-23 20:48:35 +1100

    Create data_loader.py

---
+++ data_loader.py
@@ -0,0 +1,27 @@
+from torchaudio.datasets import SPEECHCOMMANDS
+import os
+
+# The dataloder split the Speech Commands dataset according to the basic settings
+# It would download the dataset everytime
+class SubsetSC(SPEECHCOMMANDS):
+    def __init__(self, subset: str = None):
+        super().__init__("./", download=True)
+
+        def load_list(filename):
+            filepath = os.path.join(self._path, filename)
+            with open(filepath) as fileobj:
+                return [os.path.normpath(os.path.join(self._path, line.strip())) for line in fileobj]
+
+        if subset == "validation":
+            self._walker = load_list("validation_list.txt")
+        elif subset == "testing":
+            self._walker = load_list("testing_list.txt")
+        elif subset == "training":
+            excludes = load_list("validation_list.txt") + load_list("testing_list.txt")
+            excludes = set(excludes)
+            self._walker = [w for w in self._walker if w not in excludes]
+
+
+# Create training and testing split of the data. 
+train_set = SubsetSC("training")
+test_set = SubsetSC("testing")
