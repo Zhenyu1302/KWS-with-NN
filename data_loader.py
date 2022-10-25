092665501735f22899a9a5cf6b12fc27434e85b6
Zhenyu1302 <88371904+Zhenyu1302@users.noreply.github.com>
2022-10-25 16:22:51 +1100

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -21,32 +21,4 @@ class SubsetSC(SPEECHCOMMANDS):
             excludes = load_list("validation_list.txt") + load_list("testing_list.txt")
             excludes = set(excludes)
             self._walker = [w for w in self._walker if w not in excludes]
-            
-def label_to_index(word):
-    # Return the position of the word in labels
-    return torch.tensor(labels.index(word))
-
-def index_to_label(index):
-    # Return the word corresponding to the index in labels
-    # This is the inverse of label_to_index
-    return labels[index]
-
-def pad_sequence(batch):
-    # Make all tensor in a batch the same length by padding with zeros
-    batch = [item.t() for item in batch]
-    batch = torch.nn.utils.rnn.pad_sequence(batch, batch_first=True, padding_value=0.)
-    return batch
-
-def collate_fn(batch):
-    # A data tuple has the form:
-    # waveform, sample_rate, label, speaker_id, utterance_number
-    tensors, targets = [], []
-    # Gather in lists, and encode labels as indices
-    for waveform, _, label, *_ in batch:
-        tensors += [waveform]
-        targets += [label_to_index(label)]
-    # Group the list of tensors into a batched tensor
-    tensors = pad_sequence(tensors)
-    # Concatenate label indexes along a new dimension
-    targets = torch.stack(targets)
-    return tensors, targets
+           
