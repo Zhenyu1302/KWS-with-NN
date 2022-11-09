a4ff899a48da64f450e3016c355535dcd9ab00d2
Zhenyu1302 <88371904+Zhenyu1302@users.noreply.github.com>
2022-11-09 20:45:11 +1100

    Update features.py
    Adds a stacked version of feature extraction method which is compatible with RNN-related architecture, one with CTC loss or phonetic based non-streaming model

--- features.py
+++ features.py
@@ -15,3 +15,19 @@ def feature(data):
     fbank = fbank[None,:]
     fbank = torch.permute(fbank,(1,0,2,3))
     return fbank
+
+# Stack frames to form feature maps
+# Feature extraction method compatible with RNN-related or phonetic based non-streaming model
+def feature_stack(data):
+    # Extract fbank feature and feature maps
+    # Stack adjacent 40 frames
+    stacked_tuple = []
+    for i in range(data.shape[0]):
+        fbank_map = torchaudio.compliance.kaldi.fbank(data[i:i+1,:],frame_length=25.0,frame_shift=10.0,num_mel_bins=40)
+        small_frame_list = []
+        for j in range(len(fbank_map)-40+1):
+          small_frame = fbank_map[j:j+40]
+          small_frame_list += [small_frame]       
+        stacked_tuple += [torch.stack(tuple(small_frame_list))]
+    fbank = torch.stack(tuple(stacked_tuple))
+    return fbank
